8fd74a3af1d54e17552e553a8c753ff525f3a2e0
Jerry Tong <zihangtong@gmail.com>
2022-01-18 01:49:33 -0500

    commit tree 16f3680abef2bb8e99d92314ab1dd38723f034d7

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,13 @@
 class Actor < ActiveRecord::Base
-  
+    has_many :characters
+    has_many :shows, through: :characters
+
+    def full_name
+        "#{self.first_name} #{self.last_name}"
+    end
+
+    def list_roles
+        self.characters.zip(self.shows).map {|character, show| "#{character.name} - #{show.name}"}
+        # self.characters.product(self.shows).map {|character, show| "#{character.name} - #{show.name}"}
+    end
 end
